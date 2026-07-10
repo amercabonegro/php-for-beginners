5e76c508e30e127ed96f5c390da7798e1564e55d
Amer Cabonegro <amer.cabonegro@gmail.com>
2026-07-10 15:21:40 +0800

    Conditionals and Booleans

--- index.php
+++ index.php
@@ -4,13 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php
+        $name = "Dark Matter";
+        $read = false;
+
+        if ($read) {
+            $message = "You have read $name";
+        }else {
+            $message = "You have not read $name";
+        }
+    ?>
     <h1>
-        <?php 
-            $greeting = "Hello,";
-            echo "$greeting World!"; 
-        ?>
+        <?= $message; ?>
     </h1>
 </body>
 </html>
