7f2929a39047d6130377f0b7b4bd12a0a28c39b2
Amer Cabonegro <amer.cabonegro@gmail.com>
2026-07-23 16:29:44 +0800

    Page Link introduction

--- index.php
+++ index.php
@@ -1,38 +1,3 @@
 <?php
-        $books = [ 
-            [
-                'name' => 'Do androids Dream of Electric Sheep',
-                'author' => 'Philip K. Dick',
-                'purchaseUrl' => 'http://example.com/Philip',
-                'releaseYear' => 1967,
-            ],
-            [
-                'name' => 'Project Hail Mary',
-                'author' => 'Andy Weir',
-                'purchaseUrl' => 'http://example.com/Andy',
-                'releaseYear' => 2000,
-            ],
-        ];
 
-        function filter($items, $fn){
-            $filteredItems = [];
-
-            foreach ($items as $item){
-                if($fn($item)){
-                    $filteredItems[] = $item;
-                }
-            }
-
-            return $filteredItems;
-        };
-
-        $filteredBooks = filter($books, function($book){
-            return $book['releaseYear'] === 1967;
-        });
-        
-        //it can be array_filter
-        $filteredBooks2 = array_filter($books, function($book){
-            return $book['releaseYear'] === 1967;
-        });
-
-require "index.view.php";
+require "about.view.php";
