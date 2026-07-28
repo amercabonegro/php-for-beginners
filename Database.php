9190ac8b3e63046aaf0d8c7cc9bf6c4db942b6e5
Amer Cabonegro <amer.cabonegro@gmail.com>
2026-07-28 23:46:24 +0800

    Environments and Configurations

--- Database.php
+++ Database.php
@@ -3,10 +3,19 @@
 class Database
 {
     public $connection;
-    public function __construct()
+
+    public function __construct($config, $username = 'root', $password = '')
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-        $this->connection = new PDO($dsn);
+
+        //refactored dsn
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        // old dsn
+        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+
+        $this->connection = new PDO($dsn, $username, $password,[
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
     }
 
     public function query($query)
--- index.php
+++ index.php
@@ -4,10 +4,9 @@ require 'functions.php';
 
 require 'Database.php';
 
-$db = new Database();
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$config = require 'config.php';
+$db = new Database($config['database']);
+$posts = $db->query("select * from posts")->fetchAll();
 
 
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
