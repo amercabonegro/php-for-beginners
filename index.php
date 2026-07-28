1b30cfe3ed8e25e62f76cdd5c5fe4004c4116926
Amer Cabonegro <amer.cabonegro@gmail.com>
2026-07-28 18:36:07 +0800

    PDO First Steps

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 require 'functions.php';
 // require 'router.php';
 
-class Person{
-    public $name;
-    public $age;
+//connec to our MySQL database
 
-    public function breathe(){
-        echo $this->name . " is breathing";
-    }
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post){
+    echo "<li>" . $post['title'] . "</li>";
 }
-
-$person = new Person();
-
-$person->name = 'John Doe';
-$person->age = 25;
-
-$person->breathe();
