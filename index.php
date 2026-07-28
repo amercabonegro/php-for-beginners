2089f7d5d3bcefdd4d1d3fdec14d42e09b5c4800
Amer Cabonegro <amer.cabonegro@gmail.com>
2026-07-28 12:08:03 +0800

    Creation of DD - die and dump

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+
+function dd($value){
+    echo "<pre>";
+        var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+dd($_SERVER);
 $heading = "Home";
 require "views/index.view.php";
