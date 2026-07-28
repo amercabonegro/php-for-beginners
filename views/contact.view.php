6a807cd8a8c2603cd6633deec5ad1e51ff0991d2
Amer Cabonegro <amer.cabonegro@gmail.com>
2026-07-28 11:59:06 +0800

    PHP Partials

---
+++ views/contact.view.php
@@ -0,0 +1,17 @@
+<?php require "partials/head.php" ?>
+<?php require "partials/nav.php" ?>
+<?php require "partials/banner.php" ?>
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1>
+            Hello, Welcome to the contact page.
+        </h1>
+    </div>
+</main>
+</div>
+
+
+</body>
+
+</html>
